a1fc7677fe0e41be0457b22d8103239a4bf95e18
Aiden367 <aidenbyrne53@gmail.com>
2025-12-10 16:49:06 +0000

    Added styling for the Vehicles page

--- logistics-tracking-app/client/src/pages/Vehicles.jsx
+++ logistics-tracking-app/client/src/pages/Vehicles.jsx
@@ -2,6 +2,8 @@ import AddVehicleForm from '../components/layout/AddVehicleForm';
 import DisplayVehicles from '../components/layout/DisplayVehicles';
 import axios from 'axios';
 import React, { useState, useEffect, useRef } from 'react';
+import Navbar from '../components/layout/navbar.jsx';
+import '../styles/VehiclesPage.css';
 const Vehicles = () => {
 
     const [listOfVehicles, setListOfVehicles] = useState([])
@@ -18,7 +20,12 @@ const Vehicles = () => {
         retrieveVehicles();
     }, []);
     return (
-        <div>
+        <div className= "vehicles-container">
+            <Navbar/>
+            <div className ="vehicle-heading">
+                <h1>Vendors</h1>
+                <button >Add Vendors</button>
+            </div>
             <AddVehicleForm />
             <DisplayVehicles vehicles={listOfVehicles} />
         </div>
